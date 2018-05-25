0ea542ca09d8b25b506e5000e97eb03deb69236d
Nancy Jo <enciie@gmail.com>
2018-05-25 04:08:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,18 @@
 require 'pry'
 
 class Song
-  extend Memorable
-  extend Findable
-  include Paramable
+  extend Memorable :: ClassMethods
+  extend Findable :: ClassMethods
+  include Paramable :: InstanceMethods
 
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   @@songs << self
+  # end
 
   # def self.find_by_name(name)
   #   @@songs.detect{|a| a.name == name}
